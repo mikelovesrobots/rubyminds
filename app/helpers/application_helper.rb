79481d74f09e8ba4f794a220cd2e698a87317733
Mike Judge <mike@activerain.com>
2009-02-25 23:20:25 -0800

    Changed the PostsController#show view to assume that the Post#body attribute is text formatted as markdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  # Renders the given markdown-format text as html
+  #
+  #   render_markdown("*test*")
+  #   # => "<em>test</em>"
+  #
+  def render_markdown(text)
+    BlueCloth.new(text).to_html
+  end
 end
