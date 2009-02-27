8a91574e6bc609a56a8c578bc7297f29b9364015
Mike Judge <mike@activerain.com>
2009-02-27 00:36:39 -0800

    Live previews for the win\!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,26 @@ module ApplicationHelper
   def render_markdown(text)
     BlueCloth.new(text).to_html
   end
+
+  # Watches a given form element for changes and copies them to
+  # the destination element.  Also accepts an optional frequency
+  # denoting how often the field is checked for changes.
+  # (default: 0.2 [seconds])
+  #
+  #   = form.text_field :title
+  #   = live_field_preview :post_title, :preview_title
+  #
+  def live_field_preview(source_id, destination_id, frequency=0.2)
+    %{
+      <script type="text/javascript">
+        new Form.Element.Observer(
+          '#{source_id}',
+          #{frequency},  // 200 milliseconds
+          function(el, value){
+            $('#{destination_id}').update(value)
+          }
+        )
+      </script>
+    }
+  end
 end
