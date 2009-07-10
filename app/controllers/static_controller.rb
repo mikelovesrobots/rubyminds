59ed3bd34ed1520a60dbf7a259e46b477f3ec9d5
Mike Judge <mike@activerain.com>
2009-07-10 10:13:41 -0700

    Added a low-level recent_questions finder to StaticController#index.  And now that I think about it, it's not actually static if I'm doing database queries, is it?

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,6 +2,9 @@ class StaticController < ApplicationController
   layout 'default'
 
   def index
+    recent_question_ids = $redis.lrange "Meta/most-recent-questions", 0, 4
+    @recent_questions = $redis.mget(*recent_question_ids) if recent_question_ids.present?
+
     render :text => "ok", :layout => true
   end
 end
