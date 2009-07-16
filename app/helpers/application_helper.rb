130f72273d370ca8969f91f8b85ec9050cf870da
Mike Judge <mike@activerain.com>
2009-07-15 19:55:04 -0700

    Added another link to my twitter account, and changed another MiniCon Magazine reference to a RubyMinds reference

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def link_to_tweet text, tweet_id
-    link_to text, "http://twitter.com/mikelovesrobots/status/#{tweet_id}"
+    link_to text, "#{AppConfig.twitter.home_url}/status/#{tweet_id}"
   end
 end
