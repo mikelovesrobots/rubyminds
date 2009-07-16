7bd87cb2d532a996a5a6a5c8708738cdf2833499
Mike Judge <mike@activerain.com>
2009-07-16 08:54:44 -0700

    Added comments containing the recommended sizes for the different image thumbnails

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,10 @@
+#
+# image sizes:
+#
+#   - full: 300x197
+#   - featured: 280x184
+#   - small: 140x92
+#
 class Article < RedisModel
   saved_attributes :title, :subtitle, :blurb, :body, :images
 end
