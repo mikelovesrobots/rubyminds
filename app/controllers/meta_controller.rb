37eb744713063194362c2657945bedf86d96eb48
Mike Judge <mike@activerain.com>
2009-07-15 20:11:26 -0700

    The front page now displays questions when available.  [wip]

--- app/controllers/meta_controller.rb
+++ app/controllers/meta_controller.rb
@@ -3,6 +3,7 @@ class MetaController < ApplicationController
 
   def index
     @recent_questions = Question.last(4)
+    @featured_question = @recent_questions.shift
     @recent_tweets    = Tweet.last(5)
     render :text => "ok", :layout => true
   end
