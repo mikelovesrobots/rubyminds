1066b33baa458b325500b9c79377c6b19a4ea6a9
Mike Judge <mike@activerain.com>
2009-07-16 17:21:34 -0700

    Moved the interior of the MetaController#index action into a template, outside of the layout

--- app/controllers/meta_controller.rb
+++ app/controllers/meta_controller.rb
@@ -5,6 +5,5 @@ class MetaController < ApplicationController
     @recent_articles = Article.last(5)
     @featured_article = @recent_articles.shift
     @recent_tweets    = Tweet.last(5)
-    render :text => "ok", :layout => true
   end
 end
