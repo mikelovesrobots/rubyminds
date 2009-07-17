f501542dde943714cdf4941d6f682c465c75b15e
Mike Judge <mike@activerain.com>
2009-07-17 08:54:15 -0700

    Added a contact page

--- config/routes.rb
+++ config/routes.rb
@@ -33,6 +33,7 @@ ActionController::Routing::Routes.draw do |map|
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   map.root :controller => "meta"
   map.connect '/about', :controller => "meta", :action => "about"
+  map.connect '/contact', :controller => "meta", :action => "contact"
 
   # See how all your routes lay out with "rake routes"
 
