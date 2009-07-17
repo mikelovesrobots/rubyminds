4bac360200f7f63a270e084ca6388afb3e911dcb
Mike Judge <mike@activerain.com>
2009-07-16 17:21:57 -0700

    Added a MetaController#about page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,18 @@ module ApplicationHelper
   def link_to_tweet text, tweet_id
     link_to text, "#{AppConfig.twitter.home_url}/status/#{tweet_id}"
   end
+
+  # Adds the class "current" to the link if the current page fits
+  # the options for the link.  Otherwise delegates the call
+  # to the regular link_to helper.
+  #
+  def link_to_with_current name, options = {}, html_options={}
+    dup_options = html_options.dup
+    
+    if current_page?(options)
+      dup_options[:class] = "current"
+    end
+
+    link_to name, options, dup_options
+  end
 end
