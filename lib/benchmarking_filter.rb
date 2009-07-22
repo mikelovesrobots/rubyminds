9ad664eaf9dc774a1d818682e8d1b9e7d36df4ee
Mike Judge <mike@activerain.com>
2009-07-22 10:28:50 -0700

    Changed the benchmarking feature back to 2 significant digits.  The 3rd digit always seems to be a 0, so it looks like we don't have the kind of timing resolution I wanted.

--- lib/benchmarking_filter.rb
+++ lib/benchmarking_filter.rb
@@ -1,7 +1,7 @@
 class BenchmarkingFilter
   def self.filter(controller, &block)
     timing = Benchmark.measure(&block)
-    controller.response.body.sub! '<!-- rendered_in -->', "%.3f" % timing.total
+    controller.response.body.sub! '<!-- rendered_in -->', "%.2f" % timing.total
     true
   end
 end
