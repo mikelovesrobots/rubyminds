a772367679560fab775ec52d7ee168ebe828d3b7
Mike Judge <mike@activerain.com>
2009-07-24 10:00:49 -0700

    Fixed the keywords on the home page

--- app/controllers/meta_controller.rb
+++ app/controllers/meta_controller.rb
@@ -3,9 +3,9 @@ class MetaController < ApplicationController
 
   def index
     @recent_articles = Article.last(5)
+    @keywords = @recent_articles.collect { |x| x.tags }.compact.flatten
     @featured_article = @recent_articles.shift
     @recent_tweets    = Tweet.last(5)
-    @keywords = @recent_articles.collect { |x| Array(x.tags) }.flatten
   end
 
   def about
