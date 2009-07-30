4327cc75e53ffbab3ef864e30d6a067192c81487
Mike Judge <mike@activerain.com>
2009-07-30 10:16:37 -0700

    Put the last tweet in the footer of every page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,10 +10,15 @@ class ApplicationController < ActionController::Base
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
   before_filter :set_default_meta_tags
+  before_filter :load_last_tweet
 
   def set_default_meta_tags
     @keywords = []
     @description = ''
     @title = ''
+  end
+
+  def load_last_tweet
+    @last_tweet = Tweet.last(1).first
   end
 end
